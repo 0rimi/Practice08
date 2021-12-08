b965f1e66fc6d16572f7b187566d0054fa6f8c3d
이영림 <rm1113@naver.com>
2021-12-08 12:57:58 +0900

    ex06

--- src/com/javaex/ex06/PointApp.java
+++ src/com/javaex/ex06/PointApp.java
@@ -15,6 +15,12 @@ public class PointApp {
 		System.out.println(p4 == p1);		
 		System.out.println(p1.equals(p2));
 		System.out.println(p4.equals(p1));
+		
+		//==는 단순 주소값 비교.
+		//p4와 p1의 주소값은 같음.
+		//Object의 equals사용.
+		//equals 조건이 없으므로 x,y값이 같아도 같다고 보지 않음.
+				
 	}
 
 }
