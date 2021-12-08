f03075d4f6a639a6f0ba5a932c26fd7b4a6c178c
이영림 <rm1113@naver.com>
2021-12-08 12:34:20 +0900

    ex04

--- src/com/javaex/ex04/SumApp.java
+++ src/com/javaex/ex04/SumApp.java
@@ -10,10 +10,14 @@ public class SumApp {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 공백으로 구분해서 입력하세요");
 		String numLine = sc.nextLine();
-
+		
 		//코드를 작성하세요
+		//공백으르 기준으로 자르기
+		String num[] = numLine.split(" ");
 		
-		
+		for(int i=0; i<num.length; i++) {
+			sum = sum + Integer.parseInt(num[i]);
+		}		
 		
 		System.out.println("합:" + sum);
 		
